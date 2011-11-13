4579dfd0b8be122f934f3027823d6c062ea1fc0d
Sven Marnach <sven@marnach.net>
2011-11-13 02:03:15 +0000

    Rename 'Object::obj' member to 'Object::self'

--- types.hh
+++ types.hh
@@ -1,127 +1,130 @@
 #ifndef SIMWRAP_TYPES_HH
 #define SIMWRAP_TYPES_HH
 
+#include <vector>
+
+// This header contains wrappers around Python objects
+
 namespace SimWrap
 {
-    // Simple wrapper around Python objects, implicitly convertible from
-    // and to basic C++ types
+    // Wrapper around Python objects, implicitly convertible from and
+    // to basic C++ types
     class Object
     {
     public:
-        explicit Object(PyObject *obj_)
-            : obj(obj_)
+        explicit Object(PyObject *self_)
+            : self(self_)
         {
-            check_error(obj);
+            check_error(self);
         }
         Object(const Object &other)
-            : obj(other.obj)
+            : self(other.self)
         {
-            Py_INCREF(obj);
+            Py_INCREF(self);
         }
         ~Object()
         {
-            Py_DECREF(obj);
+            Py_DECREF(self);
         }
         Object(bool value)
-            : obj(PyBool_FromLong(value))
+            : self(PyBool_FromLong(value))
         {}
         Object(long value)
-            : obj(PyInt_FromLong(value))
+            : self(PyInt_FromLong(value))
         {
-            check_error(obj);
+            check_error(self);
         }
         Object(double value)
-            : obj(PyFloat_FromDouble(value))
+            : self(PyFloat_FromDouble(value))
         {
-            check_error(obj);
+            check_error(self);
         }
         Object(const char *value)
-            : obj(PyString_FromString(value))
+            : self(PyString_FromString(value))
         {
-            check_error(obj);
+            check_error(self);
         }
         operator bool() const
         {
-            return check_error(PyObject_IsTrue(obj));
+            return check_error(PyObject_IsTrue(self));
         }
         operator long() const
         {
-            return check_error(PyInt_AsLong(obj));
+            return check_error(PyInt_AsLong(self));
         }
         operator double() const
         {
-            return check_error(PyFloat_AsDouble(obj));
+            return check_error(PyFloat_AsDouble(self));
         }
         operator const char *() const
         {
-            return check_error(PyString_AsString(obj));
+            return check_error(PyString_AsString(self));
         }
         operator PyObject *() const
         {
-            return obj;
+            return self;
         }
         PyObject *new_reference() const
         {
-            Py_INCREF(obj);
-            return obj;
+            Py_INCREF(self);
+            return self;
         }
         bool callable() const
         {
-            return PyCallable_Check(obj);
+            return PyCallable_Check(self);
         }
         Object operator()() const
         {
-            return Object(PyObject_CallObject(obj, 0));
+            return Object(PyObject_CallObject(self, 0));
         }
         Object operator()(Object arg1) const
         {
-            return Object(PyObject_CallFunctionObjArgs(obj,
+            return Object(PyObject_CallFunctionObjArgs(self,
                                                        (PyObject *)arg1, 0));
         }
         Object operator()(Object arg1, Object arg2) const
         {
-            return Object(PyObject_CallFunctionObjArgs(obj,
+            return Object(PyObject_CallFunctionObjArgs(self,
                                                        (PyObject *)arg1,
                                                        (PyObject *)arg2, 0));
         }
         Object operator()(Object arg1, Object arg2, Object arg3) const
         {
-            return Object(PyObject_CallFunctionObjArgs(obj,
+            return Object(PyObject_CallFunctionObjArgs(self,
                                                        (PyObject *)arg1,
                                                        (PyObject *)arg2,
                                                        (PyObject *)arg3, 0));
         }
         Object operator()(Object arg1, Object arg2, Object arg3, Object arg4) const
         {
-            return Object(PyObject_CallFunctionObjArgs(obj,
+            return Object(PyObject_CallFunctionObjArgs(self,
                                                        (PyObject *)arg1,
                                                        (PyObject *)arg2,
                                                        (PyObject *)arg3,
                                                        (PyObject *)arg4, 0));
         }
     protected:
-        PyObject *const obj;
+        PyObject *const self;
     };
 
-    // Simple wrapper around Python mapping types
     class Mapping : public Object
     {
     public:
-        explicit Mapping(PyObject *obj_)
-            : Object(obj_)
+        explicit Mapping(PyObject *self_)
+            : Object(self_)
         {
-            if (!PyMapping_Check(obj))
+            if (!PyMapping_Check(self))
                 throw TypeError("argument must be a mapping");
         }
         Mapping(const Object &other)
             : Object(other)
         {
-            if (!PyMapping_Check(obj))
+            if (!PyMapping_Check(self))
                 throw TypeError("argument must be a mapping");
         }
         Object get(const char *key) const
         {
-            return Object(PyMapping_GetItemString(obj, const_cast<char *>(key)));
+            return Object(PyMapping_GetItemString(self, const_cast<char *>(key)));
         }
         template <typename T>
         T get(const char *key, T default_value) const
@@ -133,61 +136,60 @@ namespace SimWrap
         void set(const char *key, Object value) const
         {
             check_error(PyMapping_SetItemString(
-                            obj, const_cast<char *>(key), value));
+                            self, const_cast<char *>(key), value));
         }
         void del(const char *key) const
         {
-            check_error(PyMapping_DelItemString(obj, const_cast<char *>(key)));
+            check_error(PyMapping_DelItemString(self, const_cast<char *>(key)));
         }
         bool has_key(const char *key) const
         {
-            return PyMapping_HasKeyString(obj, const_cast<char *>(key));
+            return PyMapping_HasKeyString(self, const_cast<char *>(key));
         }
     };
 
-    // Simple wrapper around Python sequence types
     class Sequence : public Object
     {
     public:
-        explicit Sequence(PyObject *obj_)
-            : Object(obj_)
+        explicit Sequence(PyObject *self_)
+            : Object(self_)
         {
-            if (!PySequence_Check(obj))
+            if (!PySequence_Check(self))
                 throw TypeError("argument must be a sequence");
         }
         Sequence(const Object &other)
             : Object(other)
         {
-            if (!PySequence_Check(obj))
+            if (!PySequence_Check(self))
                 throw TypeError("argument must be a sequence");
         }
         Object get(ssize_t item) const
         {
-            return Object(PySequence_GetItem(obj, item));
+            return Object(PySequence_GetItem(self, item));
         }
         void set(ssize_t item, Object value) const
         {
-            check_error(PySequence_SetItem(obj, item, value));
+            check_error(PySequence_SetItem(self, item, value));
         }
         void del(ssize_t item) const
         {
-            check_error(PySequence_DelItem(obj, item));
+            check_error(PySequence_DelItem(self, item));
         }
     };
 
     class List : public Sequence
     {
     public:
-        explicit List(PyObject *obj_)
-            : Sequence(obj_)
+        explicit List(PyObject *self_)
+            : Sequence(self_)
         {
-            if (!PyList_Check(obj))
+            if (!PyList_Check(self))
                 throw TypeError("argument must be a list");
         }
         List(const Object &other)
             : Sequence(other)
         {
-            if (!PyList_Check(obj))
+            if (!PyList_Check(self))
                 throw TypeError("argument must be a list");
         }
         List()
@@ -198,35 +200,35 @@ namespace SimWrap
             : Sequence(PyList_New(v.size()))
         {
             for (size_t i = 0; i < v.size(); ++i)
-                PyList_SET_ITEM(obj, i, Object(v[i]).new_reference());
+                PyList_SET_ITEM(self, i, Object(v[i]).new_reference());
         }
         template <typename T>
         void as_std_vector(std::vector<T> &v)
         {
             v.clear();
-            v.resize(Py_SIZE(obj));
+            v.resize(Py_SIZE(self));
             for (size_t i = 0; i < v.size(); ++i)
             {
-                Object item(PyList_GET_ITEM(obj, i));
+                Object item(PyList_GET_ITEM(self, i));
                 item.new_reference();
                 v[i] = item;
             }
         }
         void insert(ssize_t index, Object value)
         {
-            check_error(PyList_Insert(obj, index, value));
+            check_error(PyList_Insert(self, index, value));
         }
         void append(Object value)
         {
-            check_error(PyList_Append(obj, value));
+            check_error(PyList_Append(self, value));
         }
         void sort()
         {
-            check_error(PyList_Sort(obj));
+            check_error(PyList_Sort(self));
         }
         void reverse()
         {
-            check_error(PyList_Reverse(obj));
+            check_error(PyList_Reverse(self));
         }
     };
 }
